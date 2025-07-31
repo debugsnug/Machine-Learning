9b17209b44925141a4f9bb4931c41ec61f58bd24
debugsnug <kiran.kishore.4414@gmail.com>
2025-07-31 15:38:38 +0530

    A1

--- LAB 4/Codes.py
+++ LAB 4/Codes.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import pickle
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
@@ -9,4 +11,54 @@ from sklearn.metrics import precision_score, recall_score, f1_score
 
 # Load the dataset
 file_path = r"D:\\OneDrive - Amrita vishwa vidyapeetham\\SEM 5\\23CSE301 Machine Learning\\LAB\\Machine-Learning\\LAB 3\\20231225_dfall_obs_data_and_spectral_features_revision1_n469.csv"
-dataset = pd.read_csv(file_path)
+data = pd.read_csv(file_path)
+
+def create_intensity_classes(df):
+    # Define thresholds based on percentiles or domain knowledge
+    low_threshold = df['max'].quantile(0.33)
+    high_threshold = df['max'].quantile(0.66)
+    
+    # Create class labels
+    conditions = [
+        (df['max'] < low_threshold),
+        (df['max'] >= low_threshold) & (df['max'] < high_threshold),
+        (df['max'] >= high_threshold)
+    ]
+    class_labels = [0, 1, 2]  # or ['Low', 'Medium', 'High']
+    
+    return np.select(conditions, class_labels)
+    
+data["class"] = create_intensity_classes(data)
+
+class_1_and_2 = data[data['class'].isin([1, 2])]
+
+# Extract features and labels again after filtering
+X = class_1_and_2[['max', 'distance_to_event']].values
+y = class_1_and_2['class'].values
+
+# Split the data into training and test sets (70% train, 30% test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+filename = "../models/lab_3_knn_model.pkl"
+loaded_model = pickle.load(open(filename, 'rb'))
+# Make predictions on the training and test sets
+y_train_pred = loaded_model.predict(X_train)
+y_test_pred = loaded_model.predict(X_test)
+
+# Confusion Matrix for both training and test sets
+train_confusion_matrix = confusion_matrix(y_train, y_train_pred)
+test_confusion_matrix = confusion_matrix(y_test, y_test_pred)
+
+# Print confusion matrices
+print("Training Confusion Matrix:")
+print(train_confusion_matrix)
+print("\nTest Confusion Matrix:")
+print(test_confusion_matrix)
+
+print("\nTraining Classification Report:")
+print(classification_report(y_train, y_train_pred))
+print("\nTest Classification Report:")
+print(classification_report(y_test, y_test_pred))
+
+print("---------------------------------------------------------------")
+
